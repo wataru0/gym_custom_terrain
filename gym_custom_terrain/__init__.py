231f9e5245fc92431a3f5d49bf1f3472876a1854
wataru0 <wataru.tv.nanao@gmail.com>
2021-01-19 01:55:59 +0900

    add

---
+++ gym_custom_terrain/__init__.py
@@ -0,0 +1,25 @@
+import gym
+from gym.envs.registration import register
+
+register(
+    id="CustomTerrainAnt-v0",
+    entry_point="gym_custom_terrain.envs.custom_terrain_ant_env:CustomTerrainAntEnv",
+)
+
+
+def custom_make(env_id: str, xml_path: str, terrain_image: str):
+    xml_file = Et.parse(xml_path)
+    root = xml_file.getroot()
+
+    for child in root:
+        if child.tag == "asset":
+            hfield = child.find("hfield")
+            # element.set('属性'，'変更後のデータ')
+            hfield.set("file", terrain_image)
+
+    # 編集内容のファイルへの書き込み
+    xml_file.write(xml_path, encoding="UTF-8")
+
+    # envの再読み込み
+    env = gym.make(env_id)
+    return env
