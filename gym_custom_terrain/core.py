a676550781bf656760880d40ede1a5cf7f2c7d49
rainbartown <rainbartown@gmail.com>
2021-01-19 16:21:58 +0900

    /tmpにコピーしたXMLファイルを編集するよう変更

--- gym_custom_terrain/core.py
+++ gym_custom_terrain/core.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import xml.etree.ElementTree as Et
 
 import gym
@@ -10,15 +11,25 @@ def get_assets_dir_abs_path() -> str:
     return os.path.join(os.path.dirname(__file__), "envs", "assets")
 
 
-def get_xml_abs_path(filename: str) -> str:
+def get_assets_xml_abs_path(filename: str) -> str:
     assets_dir_path = get_assets_dir_abs_path()
     return os.path.join(assets_dir_path, filename)
 
 
+def get_tmp_xml_abs_path(filename: str) -> str:
+    return os.path.join("/tmp", filename)
+
+
 def custom_make(env_id: str, terrain_image: str):
-    xml_path = get_xml_abs_path(env_xml_dict[env_id])
-    xml_file = Et.parse(xml_path)
-    root = xml_file.getroot()
+    xml_name = env_xml_dict[env_id]
+    assets_xml_path = get_assets_xml_abs_path(xml_name)
+
+    # 編集するのは/tmp以下のXMLファイル
+    tmp_xml_path = os.path.join("/tmp", xml_name)
+    shutil.copyfile(assets_xml_path, tmp_xml_path)
+
+    tmp_xml_file = Et.parse(tmp_xml_path)
+    root = tmp_xml_file.getroot()
 
     for child in root:
         if child.tag == "asset":
@@ -27,7 +38,7 @@ def custom_make(env_id: str, terrain_image: str):
             hfield.set("file", terrain_image)
 
     # 編集内容のファイルへの書き込み
-    xml_file.write(xml_path, encoding="UTF-8")
+    tmp_xml_file.write(tmp_xml_path, encoding="UTF-8")
 
     # envの再構成
     env = gym.make(env_id)
--- gym_custom_terrain/envs/custom_terrain_ant_env.py
+++ gym_custom_terrain/envs/custom_terrain_ant_env.py
@@ -1,12 +1,12 @@
 import numpy as np
 from gym import utils
 from gym.envs.mujoco import mujoco_env
-from ..core import get_xml_abs_path
+from ..core import get_tmp_xml_abs_path
 
 
 class CustomTerrainAntEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
-        xml_path = get_xml_abs_path("custom_terrain_ant.xml")
+        xml_path = get_tmp_xml_abs_path("custom_terrain_ant.xml")
         mujoco_env.MujocoEnv.__init__(self, xml_path, 5)
         utils.EzPickle.__init__(self)
 
